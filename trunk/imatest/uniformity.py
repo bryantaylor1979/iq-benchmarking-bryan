8dfbee225bbd99331c132816b9e9b5c6ca6152f0
bryantaylor827@gmail.com <bryantaylor827@gmail.com@b4fb6cb4-f39e-7684-3475-f3b1b18b744b>
2014-12-24 15:58:24 +0000

    step chart and uniformity now working

--- trunk/imatest/uniformity.py
+++ trunk/imatest/uniformity.py
@@ -5,29 +5,30 @@ RUN_OBJ = run_module.run_module();
 SETTINGS_OBJ = write_module_settings.write_module_settings();
 
 
-class colorcheck():
+class uniformity():
     def __init__(self):
         # Do nothing
         self.root="C:\\Program Files (x86)\\Jenkins\\jobs\\ColorCheck\\workspace\imatest\\"
         #self.root="C:\\Users\\bryantay\\Dev\\imatest\\"
-        self.imagename='C:\\Program Files (x86)\\Jenkins\\jobs\\ColorCheck\\workspace\\images\\macbeth_daylight.jpg'
+        self.imagename='C:\\Program Files (x86)\\Jenkins\\jobs\\ColorCheck\\workspace\\images\\uniformity.jpg'
         
     def run(self):    
         # params
-        ini_filename = "colour_check.ini"
+        ini_filename = "uniformity.ini"
 		
         # write ini file
         SETTINGS_OBJ.root_directory = os.path.join(self.root,"lib")
         SETTINGS_OBJ.out_file_name=ini_filename
-        SETTINGS_OBJ.default_file_name="default_colour_check.ini"
+        SETTINGS_OBJ.default_file_name="default_uniformity.ini"
         SETTINGS_OBJ.run();
          
-        RUN_OBJ.image_name = self.imagename     
+        RUN_OBJ.image_name = self.imagename  
+        RUN_OBJ.module_name = "uniformity.exe"
         RUN_OBJ.ini_filename = os.path.join(os.path.join(self.root,"lib"),ini_filename)
         RUN_OBJ.run();
         
 if __name__ == "__main__":
-    obj = colorcheck()
+    obj = uniformity()
     obj.root="C:\\Users\\bryantay\\Dev\\imatest\\"
-    obj.imagename='C:\\Users\\bryantay\\Dev\\images\\macbeth_daylight.jpg'
+    obj.imagename='C:\\Users\\bryantay\\Dev\\images\\uniformity.jpg'
     obj.run();
