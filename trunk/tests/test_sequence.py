b99d9f322d30ca5531d1a5fba45e41ca2d075755
bryantaylor827@gmail.com <bryantaylor827@gmail.com@b4fb6cb4-f39e-7684-3475-f3b1b18b744b>
2015-01-02 18:23:35 +0000

    Adding Y corner balance checks

--- trunk/tests/test_sequence.py
+++ trunk/tests/test_sequence.py
@@ -14,6 +14,7 @@ class uniformity_test_limits(object):
          self.max_color_variance = 5
          self.max_corner_Y = 0.85
          self.min_corner_Y = 0.75
+         self.max_corner_balance_Y = 5
          
 class adaptive_uniformity_test_limits(object):
 	def __init__(self):
@@ -23,6 +24,7 @@ class adaptive_uniformity_test_limits(object):
          self.Lum0.max_color_variance = 5
          self.Lum0.max_corner_Y = 0.85
          self.Lum0.min_corner_Y = 0.75
+         self.Lum0.max_corner_balance_Y = 5
          
          self.Lum1 = uniformity_test_limits;
          self.Lum1.path = Root+'/Results/';
@@ -30,6 +32,7 @@ class adaptive_uniformity_test_limits(object):
          self.Lum1.max_color_variance = 5
          self.Lum1.max_corner_Y = 0.85
          self.Lum1.min_corner_Y = 0.75
+         self.Lum1.max_corner_balance_Y = 5
          
          self.Lum2 = uniformity_test_limits;
          self.Lum2.path = Root+'/Results/';
@@ -37,6 +40,7 @@ class adaptive_uniformity_test_limits(object):
          self.Lum2.max_color_variance = 5
          self.Lum2.max_corner_Y = 0.85
          self.Lum2.min_corner_Y = 0.75
+         self.Lum2.max_corner_balance_Y = 5
          
          self.Lum3 = uniformity_test_limits;
          self.Lum3.path = Root+'/Results/';
@@ -44,6 +48,7 @@ class adaptive_uniformity_test_limits(object):
          self.Lum3.max_color_variance = 5
          self.Lum3.max_corner_Y = 0.85
          self.Lum3.min_corner_Y = 0.75
+         self.Lum3.max_corner_balance_Y = 5
          
          self.Lum4 = uniformity_test_limits;
          self.Lum4.path = Root+'/Results/';
@@ -51,6 +56,7 @@ class adaptive_uniformity_test_limits(object):
          self.Lum4.max_color_variance = 5
          self.Lum4.max_corner_Y = 0.85
          self.Lum4.min_corner_Y = 0.75
+         self.Lum4.max_corner_balance_Y = 5
          
 class colorcheck_test_limits(object):
     def __init__(self):		 
