f61f733088d6754d346a8b132abee0cd6f05ab1b
bryantaylor827@gmail.com <bryantaylor827@gmail.com@b4fb6cb4-f39e-7684-3475-f3b1b18b744b>
2015-01-02 17:41:38 +0000

    Adding Y corner checks

--- tests/lib/read_uniformity_keyresults.py
+++ tests/lib/read_uniformity_keyresults.py
@@ -4,6 +4,10 @@ class read_uniformity_keyresults :
     def __init__(self,filename='C:/Program Files (x86)/Jenkins/jobs/ColorCheck/workspace/Results/example.json'):
         # parameters
         self.cpiq_color_variability = 0;
+        self.Y_corner_UL = 0;
+        self.Y_corner_LL = 0;
+        self.Y_corner_UR = 0;
+        self.Y_corner_LR = 0;
         
         # Read file into json format
         self.results_obj = read_results.read_results(filename);
@@ -12,9 +16,16 @@ class read_uniformity_keyresults :
         
     def run(self):
         self.cpiq_color_variability = self.results_obj.getParam(self.struct,'cpiq_color_variability'); 
-        #self.Mean_Delta_C = self.results_obj.getParam(self.struct,'Mean_Delta_C'); 
-        #self.mean_cameraSat_Pct = self.results_obj.getParam(self.struct,'mean_cameraSat_Pct'); 
+        corners = self.results_obj.getParam(self.struct,'corner_lvls_Pct_UL_LL_UR_LR');
+        self.Y_corner_UL = corners[0]
+        self.Y_corner_LL = corners[1]
+        self.Y_corner_UR = corners[2]
+        self.Y_corner_LR = corners[3]
         print "cpiq_color_variability: " + str(self.cpiq_color_variability)
+        print "Y_corner_UL: " + str(self.Y_corner_UL)
+        print "Y_corner_LL: " + str(self.Y_corner_LL)
+        print "Y_corner_UR: " + str(self.Y_corner_UR)
+        print "Y_corner_LR: " + str(self.Y_corner_LR)
         
 if __name__ == "__main__":
     obj = read_uniformity_keyresults('C:/Users/bryantay/Dev/images/Results/flatfield_daylight_LF_Y.json');
