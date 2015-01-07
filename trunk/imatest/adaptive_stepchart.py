5e182811276cd3add9bd3d23000618faad890721
bryantaylor827@gmail.com <bryantaylor827@gmail.com@b4fb6cb4-f39e-7684-3475-f3b1b18b744b>
2015-01-07 14:44:39 +0000

    adaptive step chart added. (Batch process multiple colour temperatures)

---
+++ trunk/imatest/adaptive_stepchart.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 23 10:59:51 2014
+
+@author: bryantay
+"""
+
+import stepchart
+import os
+CC_OBJ = stepchart.stepchart()
+
+class adaptive_uniformity():
+    def __init__(self):
+        # Do nothing
+        self.root="C:\\Program Files (x86)\\Jenkins\\jobs\\ColorCheck\\workspace\\"
+        #self.root="C:\\Users\\bryantay\\Dev\\"
+        
+    def process_image(self,imagename):
+        CC_OBJ.root = os.path.join(self.root,'imatest')
+        CC_OBJ.imagename=os.path.join(self.root,'images',imagename)
+        CC_OBJ.run();  
+        
+    def run(self):    
+        # Params
+        self.process_image('stepchart_daylight.jpg')
+        self.process_image('stepchart_cwf.jpg')
+        self.process_image('stepchart_inc.jpg')
+        self.process_image('stepchart_horizon.jpg')
+        self.process_image('stepchart_u30.jpg')
+        
+if __name__ == "__main__":
+    obj = adaptive_uniformity()
+    obj.root="C:\\Users\\bryantay\\Dev\\"
+    obj.run();
