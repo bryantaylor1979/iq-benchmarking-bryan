63385038e4ed49384c675524e08f9a646658efa4
bryantaylor827@gmail.com <bryantaylor827@gmail.com@b4fb6cb4-f39e-7684-3475-f3b1b18b744b>
2015-01-19 19:09:55 +0000

    New Header & Manual Grey Targets Added.

--- testchart_find/image_tags/ImageInfo.m
+++ testchart_find/image_tags/ImageInfo.m
@@ -1,5 +1,7 @@
 function struct = ImageInfo()
     DefaultNum = []; 
+    
+    struct.ImageInfo.HeaderVersion = 1;
     struct.ImageInfo.GreyPatchCoords.LL.X = DefaultNum;
     struct.ImageInfo.GreyPatchCoords.LL.Y = DefaultNum;
     struct.ImageInfo.GreyPatchCoords.UL.X = DefaultNum;
@@ -9,6 +11,15 @@ function struct = ImageInfo()
     struct.ImageInfo.GreyPatchCoords.UR.X = DefaultNum;
     struct.ImageInfo.GreyPatchCoords.UR.Y = DefaultNum;
 
+    struct.ImageInfo.ManualGreyPatchCoords.LL.X = DefaultNum;
+    struct.ImageInfo.ManualGreyPatchCoords.LL.Y = DefaultNum;
+    struct.ImageInfo.ManualGreyPatchCoords.UL.X = DefaultNum;
+    struct.ImageInfo.ManualGreyPatchCoords.UL.Y = DefaultNum;
+    struct.ImageInfo.ManualGreyPatchCoords.LR.X = DefaultNum;
+    struct.ImageInfo.ManualGreyPatchCoords.LR.Y = DefaultNum;
+    struct.ImageInfo.ManualGreyPatchCoords.UR.X = DefaultNum;
+    struct.ImageInfo.ManualGreyPatchCoords.UR.Y = DefaultNum;
+    
     for i = 1:24
         struct.ImageInfo.Macbeth.(['Patch',num2str(i)]).LL.X = DefaultNum;
         struct.ImageInfo.Macbeth.(['Patch',num2str(i)]).LL.Y = DefaultNum;
