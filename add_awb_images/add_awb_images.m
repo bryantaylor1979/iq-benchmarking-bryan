01ec3123c91ffa13b29ef046b8895cf1d7b5beae
bryantaylor827@gmail.com <bryantaylor827@gmail.com@b4fb6cb4-f39e-7684-3475-f3b1b18b744b>
2015-01-21 17:36:55 +0000

    example added

--- add_awb_images/add_awb_images.m
+++ add_awb_images/add_awb_images.m
@@ -1,5 +1,14 @@
 function add_awb_images(ProcessAllImage)
     ImageRootPath = '/mnt/depart_share/ImageDatabase/AmazonAWB/tobeadded/';
     WorkspacePath = '/var/lib/jenkins/jobs/findmacbeth_amazon/workspace/';
-    disp(['Enable: ',ProcessAllImage])
+    WorkspacePath = '/home/bryan/AddAWB_Pictures/trunk/';
+    addpath(fullfile(WorkspacePath,'add_awb_images'))
+    addpath(fullfile(WorkspacePath,'testchart_find'))
+    addpath(fullfile(WorkspacePath,'testchart_find','image_tags'))
+    addpath(fullfile(WorkspacePath,'testchart_find','macbeth_chart'))
+    addpath(fullfile(WorkspacePath,'testchart_find','macbeth_chart','amazon'))
+    addpath(fullfile(WorkspacePath,'testchart_find','macbeth_chart','combined'))
+    addpath(fullfile(WorkspacePath,'testchart_find','macbeth_chart','testing'))
+    addpath(fullfile(WorkspacePath,'testchart_find','macbeth_chart','udayton'))
+    disp(['Enable: ',num2str(ProcessAllImage)])
 end
