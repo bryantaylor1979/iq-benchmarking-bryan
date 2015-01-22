8dc200f14876a0340e6dcce083849289788f6fb2
bryantaylor827@gmail.com <bryantaylor827@gmail.com@b4fb6cb4-f39e-7684-3475-f3b1b18b744b>
2015-01-22 16:26:34 +0000

    made the main function a classdef and also added some features to generate list of files

--- add_awb_images/add_awb_images.m
+++ add_awb_images/add_awb_images.m
@@ -1,14 +1,35 @@
-function add_awb_images(WorkspacePath,ProcessAllImage,PhotoshootName,ProjectName)
-    ImageDatabaseRoot = '/mnt/depart_share/ImageDatabase/'
-    ImageRootPath = fullfile(ImageDatabaseRoot,ProjectName,'tobeadded');
-    addpath(fullfile(WorkspacePath,'add_awb_images'))
-    addpath(fullfile(WorkspacePath,'testchart_find'))
-    addpath(fullfile(WorkspacePath,'testchart_find','image_tags'))
-    addpath(fullfile(WorkspacePath,'testchart_find','macbeth_chart'))
-    addpath(fullfile(WorkspacePath,'testchart_find','macbeth_chart','amazon'))
-    addpath(fullfile(WorkspacePath,'testchart_find','macbeth_chart','combined'))
-    addpath(fullfile(WorkspacePath,'testchart_find','macbeth_chart','testing'))
-    addpath(fullfile(WorkspacePath,'testchart_find','macbeth_chart','udayton'))
-    disp(['Enable: ',num2str(ProcessAllImage)]) 
-    disp(['PhotoshootName: ',PhotoshootName])
+classdef add_awb_images < handle
+    properties
+        ImageDatabaseRoot = '/mnt/depart_share/ImageDatabase/';
+    end
+    methods
+        function add_awb_images(WorkspacePath,ProcessAllImage,PhotoshootName,ProjectName)
+            ImageRootPath = fullfile(obj.ImageDatabaseRoot,ProjectName,'tobeadded');
+            
+            obj.add_all_paths(WorkspacePath);
+            
+            disp(['Enable: ',num2str(ProcessAllImage)]) 
+            disp(['PhotoshootName: ',PhotoshootName])
+            
+            Names = obj.listofimages(fullfile(ImageRootPath,PhotoshootName),'.jpg')
+        end
+        function add_all_paths(obj,WorkspacePath)
+            addpath(fullfile(WorkspacePath,'add_awb_images'));
+            addpath(fullfile(WorkspacePath,'testchart_find'));
+            addpath(fullfile(WorkspacePath,'testchart_find','image_tags'));
+            addpath(fullfile(WorkspacePath,'testchart_find','macbeth_chart'));
+            addpath(fullfile(WorkspacePath,'testchart_find','macbeth_chart','amazon'));
+            addpath(fullfile(WorkspacePath,'testchart_find','macbeth_chart','combined'));
+            addpath(fullfile(WorkspacePath,'testchart_find','macbeth_chart','testing'));
+            addpath(fullfile(WorkspacePath,'testchart_find','macbeth_chart','udayton'));            
+        end
+        function Names = listofimages(obj,Path,ImageType)
+            OBJ = ImageIO(  'Path',            Path, ...
+                            'EnableFiltering', true, ...
+                            'DirectoriesOnly', false, ...
+                            'ImageType',       ImageType);   
+            OBJ.RUN();
+            Names = OBJ.names;
+        end
+    end
 end
