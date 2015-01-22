03fc92f432f9affa058046773a1ff1c93b8e6a3f
bryantaylor827@gmail.com <bryantaylor827@gmail.com@b4fb6cb4-f39e-7684-3475-f3b1b18b744b>
2015-01-22 19:37:25 +0000

    copy files to database

--- trunk/add_awb_images/Example.m
+++ trunk/add_awb_images/Example.m
@@ -2,5 +2,6 @@
 close all
 clear classes
 ProjectName = 'AmazonAWB';
+ProcessAllImage = true
 WorkspacePath = '/home/bryan/AddAWB_Pictures/trunk/';
-obj = add_awb_images(WorkspacePath,false,'Bridge',ProjectName);
+obj = add_awb_images(WorkspacePath,ProcessAllImage,'Bridge',ProjectName);
