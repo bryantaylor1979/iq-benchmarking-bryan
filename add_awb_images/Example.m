7960930a6154ad16e0131093be09291fd03cfe5e
bryantaylor827@gmail.com <bryantaylor827@gmail.com@b4fb6cb4-f39e-7684-3475-f3b1b18b744b>
2015-01-23 17:17:57 +0000

    macbeth find processing

--- add_awb_images/Example.m
+++ add_awb_images/Example.m
@@ -4,7 +4,4 @@ clear classes
 ProjectName = 'AmazonAWB';
 ProcessAllImage = true;
 WorkspacePath = '/home/bryan/AddAWB_Pictures/trunk/';
-obj = add_awb_images(WorkspacePath,ProcessAllImage,'Bridge',ProjectName);
-
-%%
-system('mkdir /mnt/depart_share/ImageDatabase/AmazonAWB/images');
+add_awb_images(WorkspacePath,ProcessAllImage,'Bridge',ProjectName);
