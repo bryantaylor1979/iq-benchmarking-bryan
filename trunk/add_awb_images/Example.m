ba6cb70a14c61851cebece613f2cd3fcf16bd130
bryantaylor827@gmail.com <bryantaylor827@gmail.com@b4fb6cb4-f39e-7684-3475-f3b1b18b744b>
2015-01-23 17:34:07 +0000

    try to purge outputs to jenkins console

--- trunk/add_awb_images/Example.m
+++ trunk/add_awb_images/Example.m
@@ -2,6 +2,6 @@
 close all
 clear classes
 ProjectName = 'AmazonAWB';
-ProcessAllImage = true;
+ProcessAllImage = false;
 WorkspacePath = '/home/bryan/AddAWB_Pictures/trunk/';
 add_awb_images(WorkspacePath,ProcessAllImage,'Bridge',ProjectName);
